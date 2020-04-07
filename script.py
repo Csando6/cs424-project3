23305ea78a5adaba57a7bd6d1d9fbecca266fa5d
csando6 <csando6@uic.edu>
2020-04-07 16:46:40 -0500

    trying to implement split and regex expression to code

--- script.py
+++ script.py
@@ -1,16 +1,22 @@
 from os import path
+import tokenize
+import re
 
 #if your file is in the same directory:
 #filename = 'release-dates.list'
 #file = open(filename)
 
 #if your file is within LIST_files
-filepath = path.relpath("LIST_files/release-dates.list")
+filepath = path.relpath("release-dates copy.list")
 with open(filepath) as file:
 
     #get first few lines to start out:
     i = 0
-    while i < 25:
-        print(file.readline())
+    while i < 43:
+        line = file.readline()
+        sLine = re.split('\(|\)|\{|\}|:|\n',line)[:-1]
+        for j in range(len(sLine)):
+            sLine[j] = sLine[j].replace('\t','')
+        print(sLine)
         i += 1
 
