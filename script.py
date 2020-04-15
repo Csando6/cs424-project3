127f897d7bd60d83fa416570d03f4947ce6f282d
csando6 <csando6@uic.edu>
2020-04-15 13:42:18 -0500

    added filter to remove VG from rating

--- script.py
+++ script.py
@@ -11,13 +11,13 @@ from numpy.compat import unicode
 #filepath = path.relpath("release-dates copy.list")
 #filepath = "release-dates copy.list"
 filepath = "workFiles/release-dates.list"
-
-#counter = 1000
+filterList = ["VG"]
 matrix = []
+
 with open(filepath, 'r') as file:
     #itterate through file
     for line in file:
-        if(line[0] == '#' or line[0]=="'" or line[0]=="\""):
+        if(line[0]=="'" or line[0]=="\""):
             #do nothing
             line
         elif re.match(".+\(\d+\)\s+\(.+\)\s*[\w\s]+:.+", line):
@@ -26,8 +26,9 @@ with open(filepath, 'r') as file:
             lineS = re.split("\(|\)|\:[.]*",line)
             del lineS[2]
             #print(lineS)
-            if(len(lineS) == 5 and lineS[0][0] != "#" and lineS[0][0] != "'"):
+            if(len(lineS) == 5 and not any(elem in lineS[2] for elem in filterList) ):
                 matrix.append(lineS)
+            
     
 customHeader = ["name","year","rating","country","date"]
 dataframe = pd.DataFrame.from_records(matrix,columns=customHeader)
