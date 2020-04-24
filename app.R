fa5abd20add5de7bfc71c49733c9eacab2b9acd4
Matt Jankowski <35581471+mjanko5@users.noreply.github.com>
2020-04-23 19:46:13 -0500

    updated app.R

--- app.R
+++ app.R
@@ -1,25 +1,113 @@
+#+
+#--app.R file for Project 3 of CS 424 - Group 4-------
+#-----------------------------------------------------
+#--Amber Little------*********************************
+#--Charly Sandoval---*********************************
+#--Matt Jankowski----*********************************
+#-----------------------------------------------------
+
 library(shiny)
 library(shinydashboard)
 
-certificate <- read.csv(file = "csvFiles/certificates-cleaned.csv",sep="\t", header= TRUE)
-genres <- read.csv(file="csvFiles/genres-cleaned.csv",sep="\t", header=TRUE)
-keyWordKey <- read.csv(file="csvFiles/keywords-keys-cleaned.csv",sep=",", header=TRUE)
-keyWordMovie <- read.csv(file="csvFiles/keywords-movies-cleaned.csv",sep=",", header=TRUE)
-movie <- read.csv(file="csvFiles/movies-cleaned.csv",sep="\t",header=TRUE)
-releaseDate <- read.csv(file="csvFiles/release-dates-cleaned.csv",sep="\t",header=TRUE)
-runningTimes <- read.csv(file="csvFiles/running-times-cleaned.csv",sep="\t")
 
-########### Front-end ###########
+#IMPORTANT: app.R needs "dark_theme_mod.R" in the same directory to run well with the dark theme:
+source("dark_theme_mod.R") #connect
+
+#NOTE: the data file to be read here is first processed by our Python scripts.
+#READ IN THE DATA FILES:
+
+certificates <- read.csv(file = "csvFiles/final_csvFiles/certificates-cleaned-final.csv",sep=",", header= TRUE)
+genres <- read.csv(file="csvFiles/final_csvFiles/genres-cleaned-final.csv",sep=",", header=TRUE)
+keyWords <- read.csv(file="csvFiles/final_csvFiles/keywords-movies-cleaned-final.csv",sep=",", header=TRUE)
+movies <- read.csv(file="csvFiles/final_csvFiles/movies-cleaned-final.csv",sep=",",header=TRUE)
+releaseDates <- read.csv(file="csvFiles/final_csvFiles/release-dates-cleaned-final.csv",sep=",",header=TRUE)
+runningTimes <- read.csv(file="csvFiles/final_csvFiles/running-times-cleaned-final.csv",sep=",")
+
+
+
+#SHINY DASHBOARD:
+
 ui <- dashboardPage(
-      dashboardHeader(title="project3"),
-      dashboardSidebar(disable=FALSE,collapsed = FALSE),
-      dashboardBody()
-)
-
-########### Back-end ###########
-server <- function(input, output){
   
-}
+  #Header
+  dashboardHeader(title = "Saturday Night at the Movies"),
+  
+  #Sidebar
+  dashboardSidebar(disable = FALSE, collapsed = FALSE
+                   
+      
+    #insert inputs here
+      
+      
 
-########### Init-App ###########
-shinyApp(ui=ui, server=server)
+    
+      
+  ),
+  
+  
+  #Body
+  dashboardBody(
+    
+    dark_theme_mod,  # dark theme
+    
+    
+    
+    # APPLICATION LAYOUT COMPONENTS:
+    fluidRow(
+     
+      column(12,
+
+             
+        tabsetPanel(
+         
+          tabPanel ( "Analysis" 
+                    
+            #outputs here
+                  
+          ), # End tab1
+
+          
+                
+          tabPanel ( "About" ,
+                     
+            h2("Saturday Night at the Movies"),
+            h3("Developed By: Amber Little, Charly Sandoval, and Matt Jankowski"),
+            h4("Project 3 in CS 424 (Data Analytics / Visualization) at the University of Illinois at Chicago Spring 2020"),
+            h5("________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________"),
+            h5("* Libraries Used: "),
+            h5("* Data Source: "),
+            h5("* Created using R, RStudio, Shiny, Python, [insert theme credit here]")
+          ) # End about tab
+         
+         
+        ) #End tabsetPanel
+      ) # End column
+    ) # End fluid row
+  ) # End dash board body
+
+  
+  
+) # End dashboard page
+
+
+# SERVER SIDE:
+server <- function(input, output, session) {
+
+  
+  # REACTIVE FUNCTIONS:
+  
+  
+  
+
+  
+  
+  # BACKEND COMPONENTS:
+  
+  
+  
+  
+  
+  
+}#end server block
+
+shinyApp(ui = ui, server = server)  #use ui and server in shiny app
