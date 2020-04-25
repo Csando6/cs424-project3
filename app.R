f53d7d7692e8d9757c2f0915bdcdac57829a9dc8
Matt Jankowski <35581471+mjanko5@users.noreply.github.com>
2020-04-25 15:17:04 -0500

    arranged layout option

--- app.R
+++ app.R
@@ -8,6 +8,8 @@
 
 library(shiny)
 library(shinydashboard)
+library(devtools)        #for theme
+library(dashboardthemes) #for theme
 
 
 #IMPORTANT: app.R needs "dark_theme_mod.R" in the same directory to run well with the dark theme:
@@ -16,12 +18,12 @@ source("dark_theme_mod.R") #connect
 #NOTE: the data file to be read here is first processed by our Python scripts.
 #READ IN THE DATA FILES:
 
-certificates <- read.csv(file = "csvFiles/final_csvFiles/certificates-cleaned-final.csv",sep=",", header= TRUE)
+#certificates <- read.csv(file = "csvFiles/final_csvFiles/certificates-cleaned-final.csv",sep=",", header= TRUE)
 genres <- read.csv(file="csvFiles/final_csvFiles/genres-cleaned-final.csv",sep=",", header=TRUE)
-keyWords <- read.csv(file="csvFiles/final_csvFiles/keywords-movies-cleaned-final.csv",sep=",", header=TRUE)
-movies <- read.csv(file="csvFiles/final_csvFiles/movies-cleaned-final.csv",sep=",",header=TRUE)
-releaseDates <- read.csv(file="csvFiles/final_csvFiles/release-dates-cleaned-final.csv",sep=",",header=TRUE)
-runningTimes <- read.csv(file="csvFiles/final_csvFiles/running-times-cleaned-final.csv",sep=",")
+#keyWords <- read.csv(file="csvFiles/final_csvFiles/keywords-movies-cleaned-final.csv",sep=",", header=TRUE)
+#movies <- read.csv(file="csvFiles/final_csvFiles/movies-cleaned-final.csv",sep=",",header=TRUE)
+#releaseDates <- read.csv(file="csvFiles/final_csvFiles/release-dates-cleaned-final.csv",sep=",",header=TRUE)
+#runningTimes <- read.csv(file="csvFiles/final_csvFiles/running-times-cleaned-final.csv",sep=",")
 
 
 
@@ -54,19 +56,45 @@ ui <- dashboardPage(
     
     # APPLICATION LAYOUT COMPONENTS:
     fluidRow(
-     
-      column(12,
-
-             
         tabsetPanel(
-         
-          tabPanel ( "Analysis" 
+    
+          tabPanel ( "Analysis", 
+                          
+                     
+             column(4,
                     
-            #outputs here
-                  
-          ), # End tab1
-
-          
+                    fluidRow(       
+                        box(title = "Movies by Year", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                            plotOutput("genreBarGraph", height = 350)
+                        ),
+                        box(title = "Movies by Month", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                            plotOutput("genreBarGraph", height = 350)
+                        )
+                    )
+             ),
+             column(4,
+                    fluidRow(       
+                      box(title = "Movies by Running Time", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                          plotOutput("genreBarGraph", height = 350)
+                      ),
+                      box(title = "Movies by Certificate", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                          plotOutput("genreBarGraph", height = 350)
+                      )
+                    )
+             ),
+             column(4,
+                    fluidRow(       
+                      box(title = "Movies by Genre", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                          plotOutput("genreBarGraph", height = 350)
+                      ),
+                      box(title = "Top N Keywords", background = "black", solidHeader = TRUE, status = "primary", width= 12,
+                          plotOutput("genreBarGraph", height = 350)
+                      )
+                    )
+             )
+                     
+                     
+          ),
                 
           tabPanel ( "About" ,
                      
@@ -79,9 +107,8 @@ ui <- dashboardPage(
             h5("* Created using R, RStudio, Shiny, Python, [insert theme credit here]")
           ) # End about tab
          
-         
-        ) #End tabsetPanel
-      ) # End column
+
+      ) #End tabsetPanel
     ) # End fluid row
   ) # End dash board body
 
@@ -104,6 +131,14 @@ server <- function(input, output, session) {
   # BACKEND COMPONENTS:
   
   
+  output$genreBarGraph <- renderPlot({
+    
+    ggplot(data=genres, aes(x=genre)) + 
+      labs(x="Genre", y = "Number of Occurences") + 
+      geom_bar(stat="count", width=0.7, fill="steelblue") +
+      theme(axis.text.x=element_text(angle=55, hjust=1)) +
+      scale_y_continuous(breaks= seq(0,150000,25000))
+  }) # End bargraph3 
   
   
   
